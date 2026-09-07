a1fd16a74bd49fad38aa2ca8d6422ce77383f948
siva <sivaprakash.s4567@gmail.com>
2026-09-07 18:16:16 +0530

    implement one digit calculator
    works only when = is pressed, doesn't consider starting as 0 until entered

--- script.js
+++ script.js
@@ -35,3 +35,58 @@ function operate(n1,op,n2){
 //console.log(operate((operate(5,'*',2)),'/',3));
 
 
+let display = document.querySelector('#display');
+let keypad = document.querySelector('#keypad');
+
+let n1=null;
+let op=null;
+let n2=null;
+
+
+keypad.addEventListener('click', (e)=>{
+    
+    if (e.target.tagName ==='BUTTON'){
+        const value =e.target.textContent;
+
+        if (e.target.id ==='clear'){
+            display.textContent="0";
+            n1=null,op=null,n2=null;
+            
+        }
+
+        else{
+            if (Number(value)){
+                display.textContent=(value);
+                if(n1===null){
+                    n1=Number(value);
+                    console.log(n1);
+                }
+                else{
+                    if(n2===null){
+                        n2=Number(value);
+                        console.log(n2);
+                        
+                    }
+                }
+                
+            }
+            else{
+
+                if(value!=='='){
+                    //display.textContent(operate(n1,op,n2));
+                    console.log(value);
+                    op=value;
+                }
+                else{
+                    display.textContent=n1=operate(n1,op,n2);
+                    n2=null;
+                    console.log(n1);    
+                }
+            }
+            
+            
+        }
+    }
+})
+
+
